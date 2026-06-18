a47d00f51a0a86a30f57235212b3b535558e31a2
ViniGvdo <vinicius2006franco@gmail.com>
2026-06-18 13:46:05 -0300

    Adicionando arquivo falta.py

---
+++ package/models/falta.py
@@ -0,0 +1,13 @@
+class Falta:
+    def __init__(self, qtd, data):
+        self.qtd = qtd
+        self.data = data
+
+    def exibir_falta(self):
+        print(f"{self.data}: {self.qtd} faltas")
+
+    """
+    Implementar GETTERS E SETTERS de:
+        qtd
+        data
+    """
