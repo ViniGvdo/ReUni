10d9b51bbd63f2d06ceb837d018ff8927cd76791
ViniGvdo <vinicius2006franco@gmail.com>
2026-06-18 13:40:54 -0300

    Adicionando arquivo pessoa.py

---
+++ package/models/pessoa.py
@@ -0,0 +1,10 @@
+class Pessoa:
+    def __init__(self, nome , email = None):
+        self.nome = nome
+        self.email = email
+
+    """
+    Implementar !CONTRATOS! de GETTERS E SETTERS de:
+        nome
+        email
+    """
