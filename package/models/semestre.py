cc414f92db0a26caebd04287658bae31395b2700
ViniGvdo <vinicius2006franco@gmail.com>
2026-06-18 13:44:18 -0300

    Adicionando arquivo semestre.py

---
+++ package/models/semestre.py
@@ -0,0 +1,86 @@
+from .materia import Materia
+from .professor import Professor
+
+class Semestre:
+    def __init__(self, semestre):
+        self.semestre = semestre
+        self.materias = []
+
+    def menu_semestre(self):
+        while True:
+            print("Voce esta no menu do semestre, digite '0' para sair")
+            print("1 - Criar nova materia")
+            print("2 - Selecionar materia")
+            status = int(input("Escolha o numero da funcao que deseja executar: "))
+            match status:
+                case 0:
+                    return
+                
+                case 1:
+                    nome = input("Nome da materia: ")
+                    codigo = input("Codigo: ")
+                    carga_horaria = int(input("Carga Horaria em horas: "))
+                    horario = input("Horario: ")
+                    nome_professor = input("Nome do Professor: ")
+                    email_professor = input("Email do professor: ")
+                    siape_professor = input("Siape do professor: ")
+                    departamento_professor = input("Departamento do professor: ")
+                    professor = Professor(nome_professor, email_professor, siape_professor, departamento_professor)
+                    turma = input("Turma: ")
+                    self.criar_materia(nome, codigo, carga_horaria, horario, professor, turma)
+
+                case 2:
+                    if self.materias:
+                        print("=== Lista de Materias ===")
+                        for materia in self.materias:
+                            materia.apresentar_materia()
+                            print()
+
+                        codigo_materia = input("Digite o codigo da materia que deseja selecionar: ")
+                        if self.verificar_materia(codigo_materia):
+                            materia = self.buscar_materia(codigo_materia)
+                            materia.menu_materia()  
+                        else: 
+                            print("Codigo de Materia invalido")
+
+                    else:
+                        print("Nao ha semestres criados")
+
+                case _:
+                    print("Funcao invalida")
+
+
+    def apresentar_semestre(self):
+        print(f"{self.semestre}")
+
+    def relatorio_semestre(self):
+        print(f"=== {self.semestre} ===")
+        for materia in self.materias:
+            materia.relatorio_materia()
+        print("====== ======")
+        print()
+
+    def criar_materia(self, nome, codigo, carga_horaria, horario, professor, turma):
+        materia = Materia(nome, codigo, carga_horaria, horario, professor, turma)
+        self.materias.append(materia)
+
+    def verificar_materia(self, codigo):
+        for materia in self.materias:
+            if(materia.codigo == codigo):
+                return True
+        return False
+
+    def buscar_materia(self, codigo):
+        for materia in self.materias:
+            if(materia.codigo == codigo):
+                return materia
+
+
+    def deletar_materia(self, codigo):
+        #Implementar 
+        pass
+
+    """
+    Implementar GETTERS E SETTERS de:
+        semestre
+    """
