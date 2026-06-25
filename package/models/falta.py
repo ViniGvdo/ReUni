6e2d4410389fbeedcc06f7f36458e0ffbcb634b8
ViniGvdo <vinicius2006franco@gmail.com>
2026-06-25 13:32:14 -0300

    Adicionando GETTERS E SETTERS para a classe falta

--- package/models/falta.py
+++ package/models/falta.py
@@ -1,13 +1,35 @@
 class Falta:
-    def __init__(self, qtd, data):
+    def __init__(self, qtd:int, data:str):
         self.qtd = qtd
         self.data = data
+
+    #GETTERS E SETTERS
+
+    @property
+    def qtd(self):
+        return self.__qtd
+    
+    @qtd.setter
+    def qtd(self, value):
+        if not isinstance(value, int):
+            raise TypeError("O valor deve ser um número inteiro.")
+        if value < 0:
+            raise ValueError("O número não pode ser negativo.")
+        self.__qtd = value
+
+    @property
+    def data(self):
+        return self.__data
+    
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, str):
+            raise ValueError("O valor deve ser uma string")
+        self.__data = value
+
+    #MÉTODOS
 
     def exibir_falta(self):
         print(f"{self.data}: {self.qtd} faltas")
 
-    """
-    Implementar GETTERS E SETTERS de:
-        qtd
-        data
-    """
+    
