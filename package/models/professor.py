964184b686d38c2ee668c7a6d82ba62fabc94562
ViniGvdo <vinicius2006franco@gmail.com>
2026-06-25 13:36:10 -0300

    Adicionando GETTERS E SETTERS para a classe professor e o metodo apresentar

--- package/models/professor.py
+++ package/models/professor.py
@@ -1,16 +1,45 @@
 from .pessoa import Pessoa
 
 class Professor(Pessoa):
-    def __init__(self, nome, email, siape, departamento):
-        super().__init__(nome, email=email)
-        self.siape = siape
-        self.departamento = departamento
+    def __init__(self, nome, email, departamento):
+        self.__nome = nome
+        self.__email = email
+        self.__departamento = departamento
 
+    #GETTERS E SETTERS
 
-    """
-    Implementar GETTERS E SETTERS de:
-        nome
-        email
-        siape
-        departamento
-    """
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, value):
+        if not isinstance(value, str):
+            raise ValueError("O valor deve ser uma string")
+        self.__nome = value
+
+    @property
+    def email(self):
+        return self.__email
+    
+    @email.setter
+    def email(self, value):
+        if not isinstance(value, str):
+            raise ValueError("O valor deve ser uma string")
+        self.__email = value
+
+    @property
+    def departamento(self):
+        return self.__departamento
+    
+    @departamento.setter
+    def departamento(self, value):
+        if not isinstance(value, str):
+            raise ValueError("O valor deve ser uma string")
+        self.__departamento = value
+
+    #MÉTODOS
+
+    def apresentar(self):
+        print(f"Professor: {self.nome}")
+        print(f"Email do professor: {self.email}")
